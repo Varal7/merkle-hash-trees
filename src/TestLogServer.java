e66bbfc01c31b33372a4da0f8aadb4a410ecbd92
Varal7 <varaal@gmail.com>
2016-05-30 19:46:13 +0200

    Added tests

--- src/TestLogServer.java
+++ src/TestLogServer.java
@@ -1,40 +1,48 @@
 import java.util.LinkedList;
+import java.util.Arrays;
 
 
 public class TestLogServer {
   public static void main(String [] args) {
+    long tic,tac;
+    // LogServer l1 = new LogServer("data/1.txt");
+    // l1.append("1");
+    // l1.append("1");
+    // l1.append("1");
+    // l1.append("1");
+    // l1.tree.display();
+    //
+    // LinkedList<String> list = new LinkedList<String>();
+    // list.add("1");
+    // list.add("1");
+    // list.add("1");
+    // list.add("1");
+    // LogServer l2 = new LogServer("data/1.txt");
+    // l2.append(list);
+    // l2.tree.display();
+    // if (l1.equals(l2))
+    //     System.out.println("l1 and l2 are equal");
+    // else
+    //     System.out.println("l1 and l2 are not equal");
+
+    //
+    // tic = System.currentTimeMillis();
+    // LogServer l3 = new LogServer("data/commerce.log");
+    // tac = System.currentTimeMillis();
+    // System.out.println("l3 took " + String.valueOf(tac - tic) + "ms to load");
 
 
-    //  LogServer l1 = new LogServer("test.txt");
-    //  l1.tree.display();
 
 
-      //l2.tree.display();
-      //if (l1.equals(l2)) System.out.println("l1 equals l2");
-
-
-      LogServer l1 = new LogServer("data/1.txt");
-      l1.append("1");
-      l1.append("1");
-      l1.append("1");
-      l1.append("1");
-
-      l1.tree.display();
-
-      //
-      // LinkedList<String> list = new LinkedList<String>();
-      // list.add("1");
-      // list.add("1");
-      // list.add("1");
-      // list.add("1");
-      // LogServer l2 = new LogServer("data/1.txt");
-      // l2.append(list);
-      //
-      // l2.tree.display();
-      // if (l1.equals(l2))
-      //     System.out.println("l1 and l2 are equal");
-      // else
-      //     System.out.println("l1 and l2 are not equal");
-
+    LogServer l3 = new LogServer("data/8.txt");
+    l3.tree.display();
+    System.out.println("Audit path for 3");
+    for (byte[] path : l3.genPath(1)) {
+      System.out.println(Arrays.toString(path));
+    }
+    System.out.println("Proof for [0..5]");
+    for (byte[] path : l3.genPath(5)) {
+      System.out.println(Arrays.toString(path));
+    }
   }
 }
---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+java -classpath bin/ TestLogServer
